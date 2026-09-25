34be567991248cd0305c9e58385434a688273e5e
unknown <md4701303@gmail.com>
2026-09-25 13:19:52 +0530

    140.Word Break II

---
+++ Solutions/0101-0150/0140.Word Break II.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    unordered_map<int, vector<string>> memo;
+    unordered_set<string> dict;
+
+    vector<string> dfs(string& s, int start) {
+        if (start == s.size())
+            return {""};
+
+        if (memo.count(start))
+            return memo[start];
+
+        vector<string> result;
+
+        for (int end = start; end < s.size(); end++) {
+            string word = s.substr(start, end - start + 1);
+
+            if (dict.count(word)) {
+                vector<string> suffixes = dfs(s, end + 1);
+
+                for (string suffix : suffixes) {
+                    if (suffix.empty())
+                        result.push_back(word);
+                    else
+                        result.push_back(word + " " + suffix);
+                }
+            }
+        }
+
+        return memo[start] = result;
+    }
+
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        dict = unordered_set<string>(wordDict.begin(), wordDict.end());
+
+        return dfs(s, 0);
+    }
+};
